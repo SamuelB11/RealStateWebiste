bfbd70881c4ef5b211f7dd4f4567f08d69bbdeae
Samuel Bustos <Samuelbuspar3221@gmail.com>
2024-05-08 19:45:58 -0400

    Sign Up fully fuctional

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function SignIn() {
-    return <div>SignIn</div>;
+    return <div>SignIn
+        <Link to={"/sign-up"}>
+            <span className="text-blue-700">Sign In</span>
+        </Link>
+    </div>;
 }
