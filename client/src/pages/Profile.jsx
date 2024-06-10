ca1de86e70fbda1f71b7deb570b9301047d84f60
Samuel Bustos <Samuelbuspar3221@gmail.com>
2024-06-10 16:16:13 -0400

    Add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+
+
 export default function Profile() {
     const fileRef = useRef(null);
     const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -68,6 +70,22 @@ export default function Profile() {
             dispatch(updateUserFailure(error.message));
         }
     }
+    const handleDeleteUser = async () => {
+        try {
+            dispatch(deleteUserStart());
+            const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                dispatch(deleteUserFailure(data.message));
+                return;
+            }
+            dispatch(deleteUserSuccess(data));
+        } catch (error) {
+            dispatch(deleteUserFailure(error.message));
+        }
+    };
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
@@ -114,7 +132,12 @@ export default function Profile() {
                 </button>
 
                 <div className='flex justify-between mt-5'>
-                    <span className='text-red-900 cursor-pointer'>Delete Account</span>
+                    <span
+                        onClick={handleDeleteUser}
+                        className='text-red-700 cursor-pointer'
+                    >
+                        Delete account
+                    </span>
                     <span className='text-red-900 cursor-pointer'>Log Out</span>
                 </div>
                 <span className='text-red-900'> {error ? error : ""} </span>
